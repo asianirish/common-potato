14b66c657653c07f9f8c23143e0e5302f719e51f
asianirish <asianirish@gmail.com>
2021-09-10 12:10:10 +0200

    modify util main.cpp

--- util/main.cpp
+++ util/main.cpp
@@ -1,8 +1,17 @@
 #include <QCoreApplication>
+#include <iostream>
+
+#include "gfunc.h"
+
+using namespace std;
+using namespace util;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    string str = int64_t2string(129, 36);
+    cout << "util test: " << str << "\n";
 
     return a.exec();
 }
