0936a9f5b7dafcebacd36f8a7190654f8eb36eb0
asianirish <asianirish@gmail.com>
2021-10-08 14:33:56 +0200

    delete commented code from hi/main

--- hi/main.cpp
+++ hi/main.cpp
@@ -45,36 +45,6 @@ int main(int argc, char *argv[])
     console.run();
     QObject::connect(&console, SIGNAL(quit()), &a, SLOT(quit()));
 
-    /*
-    REGISTER_CLASS_FOR_UTIL_FACTORY(uniq::Value<QString>, uniq::UuidValue)
-    REGISTER_CLASS_FOR_UTIL_FACTORY(uniq::Value<QString>, uniq::CompactUuidValue)
-    REGISTER_CLASS_FOR_UTIL_FACTORY(uniq::Value<QString>, uniq::TimeQStringValue)
-
-    hi::Item *item = new test::Currency();
-    item->setField("code", "ETH");
-    item->setField("usdPrice", 3391.03);
-
-    qDebug() << "ITEM CLASS NAME:" << item->className();
-    qDebug() << "ITEM TO MAP:" << item->toMap();
-
-    hi::Item *itemClone = new test::Currency();
-
-    itemClone->fromMap(item->toMap());
-    qDebug() << "CLONE ITEM TO MAP:" << item->toMap();
-
-    qDebug() << "-------------------------------------------------------";
-    hi::Item *anotherItem = new test::Currency();
-    anotherItem->setField("code", "TRASH");
-    qDebug() << "ANOTHER ITEM CLASS NAME:" << anotherItem->className();
-    qDebug() << "ANOTHER ITEM TO MAP:" << anotherItem->toMap();
-
-    qDebug() << "ANOTHER ITEM USD_PRICE:" << anotherItem->field("usdPrice");
-    qDebug() << "ANOTHER ITEM EU_PRICE:" << anotherItem->field("euPrice");
-
-    anotherItem->setField("usdPrice", QVariant());
-    qDebug() << "ANOTHER ITEM USD_PRICE:" << anotherItem->field("usdPrice");
-*/
-
 //TRY IT: simpleTest();
 
 
