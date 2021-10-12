a46144f561428680bd3127c6ad4b7ceca1af6a17
asianirish <asianirish@gmail.com>
2021-10-12 15:21:07 +0200

    modify ActionNewItem::simplyAct

--- hi/test/ActionNewItem.cpp
+++ hi/test/ActionNewItem.cpp
@@ -53,12 +53,14 @@ QVariant ActionNewItem::simplyAct(const QVariantList &args)
         }
 
         {
-            hi::ItemPtr btc = hi::ItemPtr(new test::Bitcoin());
+            auto newBtc = new test::Bitcoin();
+            hi::ItemPtr btc = hi::ItemPtr(newBtc);
             qDebug() << "BTC CLASS NAMA:" << btc->className();
             btc->setField("usdPrice", 55100);
 //            btc->setField("code", QString("BTC"));
             btc->setField("specialBitcoinValue", 1337);
             result += "BTC ITEM TO MAP: " + srl->serialize(btc) + "\n";
+            result += "BTC CODE: " + btc.dynamicCast<test::Currency>()->code();
         }
 
 
