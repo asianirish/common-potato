f29fb7a339bef257a076f8f7623223ab651e63ff
asianirish <asianirish@gmail.com>
2021-10-28 16:37:31 +0200

    modify TestEssenceA::execute

--- hierhin/Test/TestEssenceA.cpp
+++ hierhin/Test/TestEssenceA.cpp
@@ -11,7 +11,6 @@ TestEssenceA::TestEssenceA()
 
 void TestEssenceA::execute(hierhin::Item *item)
 {
-    Q_UNUSED(item);
     int value = item->property("value").toInt();
     item->setProperty("value", value * 2);
 }
