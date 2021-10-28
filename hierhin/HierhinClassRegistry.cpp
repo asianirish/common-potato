3d60fdf5d0c01e8906148cf580e4dd8b1116477d
asianirish <asianirish@gmail.com>
2021-10-28 15:59:42 +0200

    modify HierhinClassRegistry::registerAllClasses

--- hierhin/HierhinClassRegistry.cpp
+++ hierhin/HierhinClassRegistry.cpp
@@ -1,4 +1,11 @@
 #include "HierhinClassRegistry.h"
+#include "Test/TestEssenceA.h"
+#include "Test/TestEssenceB.h"
+
+
+#include <util/Factory.h>
+
+#include <QString>
 
 HierhinClassRegistry::HierhinClassRegistry()
 {
@@ -7,5 +14,8 @@ HierhinClassRegistry::HierhinClassRegistry()
 
 void HierhinClassRegistry::registerAllClasses() const
 {
+    REGISTER_CLASS_FOR_UTIL_FACTORY(hierhin::Essence, TestEssenceA);
+    REGISTER_CLASS_FOR_UTIL_FACTORY(hierhin::Essence, TestEssenceB);
+
     //TODO: add your code here
 }
