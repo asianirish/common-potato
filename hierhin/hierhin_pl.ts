d59e3a408725160ff2e0f789315cec198a6924fd
asianirish <asianirish@gmail.com>
2021-11-30 15:11:04 +0100

    modify hierhin .ts files

--- hierhin/hierhin_pl.ts
+++ hierhin/hierhin_pl.ts
@@ -4,15 +4,15 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="main.cpp" line="25"/>
+        <location filename="main.cpp" line="28"/>
         <source>hi</source>
         <translation type="unfinished">cześć</translation>
     </message>
     <message numerus="yes">
-        <location filename="main.cpp" line="31"/>
-        <location filename="main.cpp" line="36"/>
-        <location filename="main.cpp" line="41"/>
-        <location filename="main.cpp" line="46"/>
+        <location filename="main.cpp" line="34"/>
+        <location filename="main.cpp" line="39"/>
+        <location filename="main.cpp" line="44"/>
+        <location filename="main.cpp" line="49"/>
         <source>%n object(s)</source>
         <translation type="unfinished">
             <numerusform>%n obiekt</numerusform>
@@ -21,12 +21,12 @@
         </translation>
     </message>
     <message>
-        <location filename="main.cpp" line="52"/>
+        <location filename="main.cpp" line="55"/>
         <source>pi is %1 of conventional units</source>
         <translation type="unfinished">wartość pi wynosi %1 jednostek warunkowych</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="59"/>
+        <location filename="main.cpp" line="62"/>
         <source>wrong email format</source>
         <translation type="unfinished">zły format adresu e-mail</translation>
     </message>
