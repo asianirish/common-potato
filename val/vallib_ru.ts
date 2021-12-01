08383be5aabdc914ee0d521305edfbb67b9f1435
asianirish <asianirish@gmail.com>
2021-12-01 15:20:55 +0100

    add Russian translations

--- val/vallib_ru.ts
+++ val/vallib_ru.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="Email.cpp" line="12"/>
         <source>hi</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">привет</translation>
     </message>
     <message>
         <location filename="Email.cpp" line="13"/>
         <source>wrong email format</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">неверный формат электронной почты</translation>
     </message>
 </context>
 <context>
@@ -20,42 +20,42 @@
         <location filename="Length.cpp" line="48"/>
         <source>the string must contain more than %n characters</source>
         <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+            <numerusform>строка должна содержать более %n символа</numerusform>
+            <numerusform>строка должна содержать более %n символов</numerusform>
+            <numerusform>строка сдолжна содержать более %n символов</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="Length.cpp" line="52"/>
         <source>the string must contain less than %n characters</source>
         <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+            <numerusform>строка должна содержать менее %n символа</numerusform>
+            <numerusform>строка должна содержать менее %n символов</numerusform>
+            <numerusform>строка должна содержать менее %n символов</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="Length.cpp" line="56"/>
         <source>the string must contain more than %n</source>
         <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+            <numerusform>строка должна содержать более %n</numerusform>
+            <numerusform>строка должна содержать более %n</numerusform>
+            <numerusform>строка должна содержать более %n</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="Length.cpp" line="57"/>
         <source>and less then %n characters</source>
         <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+            <numerusform>и менее %n символа</numerusform>
+            <numerusform>и менее %n символов</numerusform>
+            <numerusform>и менее %n символов</numerusform>
         </translation>
     </message>
     <message>
         <location filename="Length.cpp" line="61"/>
         <source>wrong length validator format</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">некорректный формат валидатора</translation>
     </message>
 </context>
 <context>
@@ -63,7 +63,7 @@
     <message>
         <location filename="List.cpp" line="22"/>
         <source>should be one of the specified values</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">должен быть одним из следующих значений</translation>
     </message>
 </context>
 <context>
@@ -71,7 +71,7 @@
     <message>
         <location filename="NotEqualTo.cpp" line="31"/>
         <source>should not be equal to</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">не должно быть равно</translation>
     </message>
 </context>
 <context>
@@ -79,7 +79,7 @@
     <message>
         <location filename="Phone.cpp" line="12"/>
         <source>wrong phone number format</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">неверный формат номера телефона</translation>
     </message>
 </context>
 </TS>
