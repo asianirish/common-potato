fbe81d4b671c6f3426a5eacf477de470a7f6bb89
asianirish <asianirish@gmail.com>
2021-12-03 14:58:09 +0100

    modify Range<T>::errorMessage

--- val/Range.h
+++ val/Range.h
@@ -50,7 +50,7 @@ bool Range<T>::validate(const QVariant &value) const
 template <typename T>
 QString Range<T>::errorMessage() const
 {
-    return QString("the value should be within a range of %1 and %2").arg(_minValue.toString(), _maxValue.toString());
+    return tr("the value should be within a range of %1 and %2").arg(_minValue.toString(), _maxValue.toString());
 }
 
 template <typename T>
