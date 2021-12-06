1a51e97032497dd9276f14c3a1fbe688eaf4a8f9
asianirish <asianirish@gmail.com>
2021-12-06 14:17:51 +0100

    test childByRole

--- hierhin/Menu/NodeTest.cpp
+++ hierhin/Menu/NodeTest.cpp
@@ -41,26 +41,27 @@ QVariant NodeTest::simplyAct(const QVariantList &args)
 
         int nnn;
         QTextStream qtin(stdin);
-        qDebug() << "enter nd1 value:";
-        nnn = menu::Console::readInt();
+//        qDebug() << "enter nd1 value:";
+        nnn = 13; //menu::Console::readInt();
         nd1->setProperty("value", nnn);
 
-        qDebug() << "enter nd1 email:";
-        QString email = qtin.readLine();
+//        qDebug() << "enter nd1 email:";
+        QString email = "someone@same.com"; //qtin.readLine();
         nd1->setProperty("email", email);
 
-        qDebug() << "enter nd1 phone:";
-        QString phone = qtin.readLine();
+//        qDebug() << "enter nd1 phone:";
+        QString phone = "+49000333444"; //qtin.readLine();
         nd1->setProperty("phone", phone);
 
-        qDebug() << "enter nd2 value:";
-        nnn = menu::Console::readInt();
+//        qDebug() << "enter nd2 value:";
+        nnn = 37; //menu::Console::readInt();
         nd2->setProperty("value", nnn);
+
         //TODO: uncomment this line to test a NoSuchProperty exception
 //        nd1->setProperty("color", "red");
 
     //    nd2->setParentNode(nd1.data());
-        nd1->addChild(nd2);
+        nd1->addChild(nd2, "left");
 
         value += "NODE1 ID: " + nd1->id() + "\n";
         value += "NODE2 ID: " + nd2->id() + "\n";
@@ -68,7 +69,7 @@ QVariant NodeTest::simplyAct(const QVariantList &args)
 
 
         nd3->setProperty("value", 1337);
-        nd1->addChild(nd3);
+        nd1->addChild(nd3, "right");
 
         if (nd1->containsId(nd2->id())) {
             value += "NODE1 CONTAINS NODE2\n";
@@ -88,7 +89,14 @@ QVariant NodeTest::simplyAct(const QVariantList &args)
         nd2->execute();
         nd3->execute();
 
-        value += "NODE1 AS A JSON: " + nd1->toJson();
+        value += "NODE1 AS A JSON: " + nd1->toJson() + "\n";
+
+        auto leftNode = nd1->childByRole("left");
+        value += "LEFT NODE AS JSON: " + leftNode->toJson() + "\n";
+
+        auto rightNode = nd1->childByRole("right");
+        value += "RIGHT NODE AS JSON: " + rightNode->toJson() + "\n";
+
     }  catch (ex::Exception &e) {
         qDebug() << "AN ERROR HAS OCCURRED:" << e.cause();
     }
