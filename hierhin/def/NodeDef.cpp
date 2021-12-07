ff66dcef5fa8e2ac842691cf1aba8b4364fbd015
asianirish <asianirish@gmail.com>
2021-12-07 16:06:00 +0100

    add NodeDef::_role

--- hierhin/def/NodeDef.cpp
+++ hierhin/def/NodeDef.cpp
@@ -23,5 +23,20 @@ void NodeDef::setChildEssenceClassName(const QString &className, const Role &rol
     _childEssenceClassNames.insert(role, className);
 }
 
+const QList<Role> &NodeDef::roles() const
+{
+    return _roles;
+}
+
+void NodeDef::setRoles(const QList<Role> &newRoles)
+{
+    _roles = newRoles;
+}
+
+void NodeDef::addRole(const Role &role)
+{
+    _roles.append(role);
+}
+
 } // namespace def
 } // namespace hierhin
--- hierhin/def/NodeDef.h
+++ hierhin/def/NodeDef.h
@@ -18,9 +18,14 @@ public:
     void setChildEssenceClassNames(const QMap<Role, QString> &newChildEssenceClassNames);
     void setChildEssenceClassName(const QString &className, const Role &role = "");
 
+    const QList<Role> &roles() const;
+    void setRoles(const QList<Role> &newRoles);
+    void addRole(const Role &role);
+
 private:
     //maps roles onto class names
     QMap<Role, QString> _childEssenceClassNames;
+    QList<Role> _roles;
 };
 
 } // namespace def
