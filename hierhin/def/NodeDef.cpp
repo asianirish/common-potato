8ad70d5c6a3be34faf5b35e30ca8e4aa5e11598a
asianirish <asianirish@gmail.com>
2021-12-07 15:05:15 +0100

    add NodeDef::_childEssenceClassNames getter and setters

--- hierhin/def/NodeDef.cpp
+++ hierhin/def/NodeDef.cpp
@@ -8,5 +8,20 @@ NodeDef::NodeDef()
 
 }
 
+const QMap<Role, QString> &NodeDef::childEssenceClassNames() const
+{
+    return _childEssenceClassNames;
+}
+
+void NodeDef::setChildEssenceClassNames(const QMap<Role, QString> &newChildEssenceClassNames)
+{
+    _childEssenceClassNames = newChildEssenceClassNames;
+}
+
+void NodeDef::setChildEssenceClassName(const QString &className, const Role &role)
+{
+    _childEssenceClassNames.insert(role, className);
+}
+
 } // namespace def
 } // namespace hierhin
