03e1b8b45dc3d5e32a1ee2e72a417a4714066dc0
asianirish <asianirish@gmail.com>
2021-12-10 11:47:00 +0100

    delete NoSuchRoleExceptin::role & NoSuchRoleExceptin::setRole

--- hierhin/ex/NoSuchRoleExceptin.cpp
+++ hierhin/ex/NoSuchRoleExceptin.cpp
@@ -8,15 +8,5 @@ NoSuchRoleExceptin::NoSuchRoleExceptin()
 
 }
 
-const QString &NoSuchRoleExceptin::role() const
-{
-    return _role;
-}
-
-void NoSuchRoleExceptin::setRole(const QString &newRole)
-{
-    _role = newRole;
-}
-
 } // namespace ex
 } // namespace hierhin
