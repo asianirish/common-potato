19efe2ac29bd9dbe19e8ed61d2fbf4ff29a8e807
asianirish <asianirish@gmail.com>
2021-12-20 13:58:20 +0100

    modify QString cause() const override {

--- val/MoreThanOrEqualTo.h
+++ val/MoreThanOrEqualTo.h
@@ -53,7 +53,7 @@ bool MoreThanOrEqualTo<T>::validate(const QVariant &value) const
 template <typename T>
 QString MoreThanOrEqualTo<T>::errorMessage() const
 {
-    return tr("the value should be more than or equal to %n", "", + _value.toInt());
+    return tr("the value should be more than or equal to %n", "", _value.toInt());
 }
 
 template <typename T>
