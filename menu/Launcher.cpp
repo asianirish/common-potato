c9e8c8f80f670e3223bcdaa6e8edc2aae1f1e17f
asianirish <asianirish@gmail.com>
2022-01-01 23:08:53 +0100

    add Launcher::setTaskIdGenClassName

--- menu/Launcher.cpp
+++ menu/Launcher.cpp
@@ -41,4 +41,9 @@ void Launcher::setContextSetter(ContextSetter *contextSetter)
     _contextSetter = contextSetter;
 }
 
+void Launcher::setTaskIdGenClassName(const QString &className)
+{
+    _actionIdGen.setClassName(className);
+}
+
 } // namespace menu
