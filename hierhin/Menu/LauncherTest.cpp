6c53b59af1fd429d4c8f4805533f86285251784a
asianirish <asianirish@gmail.com>
2022-01-02 23:37:25 +0100

    modify LauncherTest::simplyAct

--- hierhin/Menu/LauncherTest.cpp
+++ hierhin/Menu/LauncherTest.cpp
@@ -1,6 +1,7 @@
 #include "LauncherTest.h"
 
 #include <menu/SyncLauncher.h>
+#include <menu/ThreadLauncher.h>
 
 #include <QDebug>
 
@@ -15,10 +16,15 @@ QVariant LauncherTest::simplyAct(const QVariantList &args)
 {
     Q_UNUSED(args)
 
-    SyncLauncher syncLauncher;
-    connect(&syncLauncher, &SyncLauncher::ready, this, &LauncherTest::onReady);
-    syncLauncher.launch("menu::math::Inc", {12});
-    syncLauncher.launch("menu::math::Div", {13, 4});
+    ThreadLauncher threadLauncher;
+    connect(&threadLauncher, &ThreadLauncher::ready, this, &LauncherTest::onReady);
+    threadLauncher.launch("menu::math::Inc", {120});
+    threadLauncher.launch("menu::math::Div", {132, 4});
+
+//    SyncLauncher syncLauncher;
+//    connect(&syncLauncher, &SyncLauncher::ready, this, &LauncherTest::onReady);
+//    syncLauncher.launch("menu::math::Inc", {12});
+//    syncLauncher.launch("menu::math::Div", {13, 4});
 
     return true;
 }
