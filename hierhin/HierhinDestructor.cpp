90e8c1d06f0f8fce3529f90e01a36c0a40594341
asianirish <asianirish@gmail.com>
2022-01-04 16:06:17 +0100

    modify HierhinDestructor::cleanUp

--- hierhin/HierhinDestructor.cpp
+++ hierhin/HierhinDestructor.cpp
@@ -1,5 +1,7 @@
 #include "HierhinDestructor.h"
 
+#include <menu/ContextSetter.h>
+
 #include <QDebug>
 
 HierhinDestructor::HierhinDestructor(QCoreApplication *app, menu::Menu *menu, menu::Launcher *launcher) : AppDestructor(app),
@@ -17,6 +19,8 @@ HierhinDestructor::~HierhinDestructor()
 void HierhinDestructor::cleanUp()
 {
     qDebug() << "CLEANING UP...";
+    menu::ContextSetter *cntx = _launcher->contextSetter();
+    delete cntx;
     delete _menu;
     delete _launcher;
 }
