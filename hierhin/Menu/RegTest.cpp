4ff2949d876ec527417f0928cdb5b9fa0a53411d
asianirish <asianirish@gmail.com>
2022-01-07 19:53:18 +0100

    fix RegTest::simplyAct

--- hierhin/Menu/RegTest.cpp
+++ hierhin/Menu/RegTest.cpp
@@ -18,10 +18,10 @@ QVariant RegTest::simplyAct(const QVariantList &args)
     Q_UNUSED(args)
 
     ObjectRegistry<int, menu::Launcher>::createObject<menu::SyncLauncher>(0);
-    ObjectRegistry<int, menu::Launcher>::createObject<menu::ThreadLauncher>(0);
+    ObjectRegistry<int, menu::Launcher>::createObject<menu::ThreadLauncher>(1);
 
     auto lnch0 = ObjectRegistry<int, menu::Launcher>::ptr(0);
-    auto lnch1 = ObjectRegistry<int, menu::Launcher>::ptr(0);
+    auto lnch1 = ObjectRegistry<int, menu::Launcher>::ptr(1);
 
     lnch0->launch("menu::math::Inc", {12}, nullptr);
     lnch1->launch("menu::math::Div", {12, 3}, nullptr);
