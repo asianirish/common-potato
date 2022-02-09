edb70fe89e3fb80d5d9a6b6e913816695d2d8d4a
asianirish <asianirish@gmail.com>
2022-02-09 16:52:31 +0100

    modify test_nn/main

--- test_nn/main.cpp
+++ test_nn/main.cpp
@@ -1,14 +1,35 @@
 #include <QCoreApplication>
 
 #include <iostream>
+#include <QSharedPointer>
+
+#include <hierhin/type.h>
+#include <hierhin/cute/NodeImpl.h>
+
+#include <nn/Neuron.h>
 
 
 using namespace std;
+using namespace nn;
+
+using namespace hierhin;
+using namespace hierhin::nav;
+using namespace hierhin::cute;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     cout << "NN project" << endl;
+
+//    auto neuron1 = hierhin::EssencePtr(new Neuron());
+////    auto neuron2 = hierhin::EssencePtr(new Neuron());
+
+//    cout << neuron1->toJson();
+
+    auto nd = NodePtr(new NodeHashImpl());
+
+    nd->toJson();
+
 
     return a.exec();
 }
