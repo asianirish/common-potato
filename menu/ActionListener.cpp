cf20ca6ce3f414c2fc3c68f81ca70a27d28dbe3b
asianirish <asianirish@gmail.com>
2022-03-09 13:32:55 +0100

    modify menu::ActionListener::handleResult

--- menu/ActionListener.cpp
+++ menu/ActionListener.cpp
@@ -9,7 +9,13 @@ ActionListener::ActionListener(QObject *parent) : QObject(parent)
 
 void menu::ActionListener::handleResult(const menu::Result &result)
 {
-    onReadyImpl(result);
+    if (result.isError()) {
+        onErrorImpl(result.error());
+        //TODO: emit error(result.error()); ?
+    } else {
+        onReadyImpl(result);
+        //TODO: emit ready(result); ?
+    }
     emit handled(result);
 }
 
