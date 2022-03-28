21b5621d21df2f0a7112ffdd427837789b377c16
asianirish <asianirish@gmail.com>
2022-03-28 16:22:38 +0200

    implement FoldTest::simplyAct

--- hierhin/Menu/FoldTest.cpp
+++ hierhin/Menu/FoldTest.cpp
@@ -1,6 +1,9 @@
 #include "FoldTest.h"
 
+#include <menu/SyncLauncher.h>
+
 #include <QDebug>
+#include <QVariant>
 
 FoldTest::FoldTest()
 {
@@ -12,6 +15,9 @@ QVariant FoldTest::simplyAct(const QVariantList &args)
     Q_UNUSED(args)
 
     qDebug() << "FOLD TEST";
+
+    menu::SyncLauncher launcher;
+    launcher.launch("menu::hof::Fold", {"menu::math::Sum", 1,2,3}, menu::ContextSetterPtr());
 
     return true;
 }
